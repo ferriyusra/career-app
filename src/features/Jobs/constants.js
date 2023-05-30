2a3329e01e321fb16f7b416c16ff23eed9f4837a
ferriyusra <ferisakuragi@gmail.com>
2023-05-30 15:19:04 +0700

    refactor

--- src/features/Jobs/constants.js
+++ src/features/Jobs/constants.js
@@ -1,6 +1,6 @@
 export const START_FETCHING_JOB = "features/Jobs/START_FETCHING_JOB";
 export const ERROR_FETCHING_JOB = "features/Jobs/ERROR_FETCHING_JOB";
-export const SUCCESS_FETCHING_JOB = "features/Job/SUCCESS_FETCHING_JOB";
+export const SUCCESS_FETCHING_JOB = "features/Jobs/SUCCESS_FETCHING_JOB";
 export const SET_PER_PAGE = "features/Jobs/SET_PER_PAGE";
 export const SET_KEYWORD = "features/Jobs/SET_KEYWORD";
 export const NEXT_PAGE = "features/Jobs/NEXT_PAGE";
